33458c41b83b017b6ed777317744fbd369056ec3
zoryzhang <hereiszory@gmail.com>
2024-06-03 13:43:42 -0400

    add: functions for printing CachedString-related objects

--- cobweb/cached_string.hpp
+++ cobweb/cached_string.hpp
@@ -60,4 +60,30 @@ namespace std {
     };
 }
 
+// functions for printing CachedString-related objects
+template <typename V>
+std::ostream& operator<<(std::ostream& os, const std::unordered_map<CachedString, V>& map) {
+    os << "{";
+    for (auto it = map.begin(); it != map.end(); ++it) {
+        if (it != map.begin()) {
+            os << ", ";
+        }
+        os << "\"" << it->first.get_string() << "\": " << it->second;
+    }
+    os << "}";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const std::unordered_set<CachedString>& set) {
+    os << "{";
+    for (auto it = set.begin(); it != set.end(); ++it) {
+        if (it != set.begin()) {
+            os << ", ";
+        }
+        os << "\"" << it->get_string() << "\"";
+    }
+    os << "}";
+    return os;
+}
+
 #endif // CACHED_STRING_H
